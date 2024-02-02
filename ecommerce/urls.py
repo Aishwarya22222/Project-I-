f24d1eaf48aa7c7c0692a6bf6e6606e1bb606fc5
Aishwarya22222 <154801337+Aishwarya22222@users.noreply.github.com>
2024-02-02 20:45:12 +0545

    urls,products app,views and assign products app in setting

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.http import HttpResponse
 def index(request):
     return HttpResponse('this is a custom url page')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/',index)
+    path('test/',index),
+    path('products/',include('products.urls'))
 ]
---
+++ products/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . views import index # . same directory cha vane // different folder huda chai . lekhera folder ko naam raakhnu parcha
+
+
+urlpatterns = [
+    path('',index)
+]
