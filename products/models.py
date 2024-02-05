64c13228c5adc130317989139b9c443915857f2b
Aishwarya22222 <154801337+Aishwarya22222@users.noreply.github.com>
2024-02-05 21:35:51 +0545

    product model, register model in admin site, insert product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name=models.CharField(max_length=100)
+    product_price=models.FloatField()
+    stock=models.IntegerField()
+    product_description=models.TextField()
+    product_image=models.CharField(max_length=225)
+    created_at = models.DateTimeField(auto_now_add= True)
+
+    def __str__(self):
+        return 
--- products/views.py
+++ products/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def index(request):
      return HttpResponse('This is from the product app')
+
+def showproduct(request):
+     products = Product.objects.all()
+     context = {
+          'products':products
+     }
