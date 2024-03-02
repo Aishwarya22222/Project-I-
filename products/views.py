4690bed10c4514e023d42212ec0ca9f40f2aaa4c
Aishwarya22222 <154801337+Aishwarya22222@users.noreply.github.com>
2024-03-02 15:45:16 +0545

    using crispy form to create addcategory form, adding category (post category)

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import*
 from .forms import CategoryForm
+from django.contrib import messages # yo chai messages ko laagi predefined huncha
 
 # Create your views here.
 def index(request):
@@ -15,10 +16,22 @@ def showproduct(request):
      }
      return render(request,'products/product.html',context)
 
-# to sow category form and post category
+# to show category form and post category
 def addCategory(request):
+     # data processing
+     if request.method == 'POST':
+          form = CategoryForm(request.POST)
+          if form.is_valid():
+               form.save()
+               messages.add_message(request, messages.SUCCESS,'Category added successfully')
+               return redirect('/products/addcategory')
+          else:
+               messages.add_message(request,messages.ERROR,'Failed to add category')
+               return render(request,'products/addcategory.html',{'forms':form}) 
 
      context={
           'forms':CategoryForm
      }
+
+     #to load add category form
      return render(request,'products/addcategory.html',context)
